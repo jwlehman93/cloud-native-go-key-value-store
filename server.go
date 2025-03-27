34df105b9b79db4ad62780fb30c49bceb73e53d9
jeremy lehman <jeremy.lehman@onxmaps.com>
2025-03-27 11:15:02 -0400

    Add gorilla handlers

--- server.go
+++ server.go
@@ -1,19 +1,72 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func helloGoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello net/http!")
 }
 
+func putHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+	err = Put(key, string(value))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	err := Delete(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	log.Printf("DELETE key=%s\n", key)
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value, err := Get(key)
+	if errors.Is(ErrorNoSuchKey, err) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, value)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", helloGoHandler)
+	r.HandleFunc("/v1/key/{key}", putHandler).Methods("PUT")
+	r.HandleFunc("/v1/key/{key}", getHandler).Methods("GET")
+	r.HandleFunc("/v1/key/{key}", deleteHandler).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
